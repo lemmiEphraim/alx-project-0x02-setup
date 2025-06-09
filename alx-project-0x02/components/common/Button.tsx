854bd10ac6e6fc45b78ce62dcd222e3bbd388a1a
lemmiEphraim <lemmiabdissa@gmail.com>
2025-06-10 01:02:43 +0300

    Update Button.tsx

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,6 +1,6 @@
 // components/common/Button.tsx
 import React from "react";
-import { ButtonProps } from "../../interfaces";
+import {type ButtonProps } from "../../interfaces";
 
 const Button: React.FC<ButtonProps> = ({
   children,
