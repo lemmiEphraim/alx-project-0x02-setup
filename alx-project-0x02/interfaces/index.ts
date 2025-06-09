c72db10e7af0b169926fd631aeae109efa1c96c2
lemmiEphraim <lemmiabdissa@gmail.com>
2025-06-09 12:03:46 +0300

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,23 @@
+// interfaces/index.ts
+export interface CardProps {
+  title: string;
+  content: string;
+  className?: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  className?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
 
